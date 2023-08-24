6e88fa5518859557921bed43986e32a18f45becb
Vetle de Vries Brandth <vetle1608@gmail.com>
2023-08-24 15:28:56 +0200

    apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { gql, useSubscription } from "@apollo/client";
+
+const subVehicles = gql`
+  subscription {
+    vehicles {
+      lastUpdated
+      vehicleId
+      line {
+        lineRef
+      }
+      location {
+        latitude
+        longitude
+      }
+    }
+  }
+`;
+
+const vehicleQuery = gql`
+  query Query {
+    vehicles {
+      vehicleId
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const asd = useSubscription(subVehicles);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <pre>{JSON.stringify(asd.data, null, 2)}</pre>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
