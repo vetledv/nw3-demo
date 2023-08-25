bbf90305565f69fcdeec2ccfbcaa3a0e00c53336
Henrik Bråthen <henrik.brathen@bouvet.no>
2023-08-25 15:05:20 +0200

    add map from prev repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import { useSubscription } from "@apollo/client";
 import { subVehicles } from "~/graphql/queries";
+import MapContainer from "./components/MapContainer";
 
 export function App() {
   const vehiclesSub = useSubscription(subVehicles);
   //TODO: filter duplicates, keep newest entry
   return (
     <main>
-      <h2>Subscriptions</h2>
-      <ul className="space-y-1">
-        {vehiclesSub.data?.vehicles?.map((vehicle) => (
-          <li key={vehicle?.vehicleId} className="p-1 bg-zinc-200">
-            {/* <p>{vehicle?.vehicleId}</p>
-            <time>{new Date(vehicle?.lastUpdated).toLocaleTimeString()}</time> */}
-            <pre>{JSON.stringify(vehicle, null, 2)}</pre>
-          </li>
-        ))}
-      </ul>
+      <MapContainer />
     </main>
   );
 }
---
+++ src/components/MapContainer.tsx
@@ -0,0 +1,98 @@
+import Map, { Marker, LngLat, MarkerDragEvent } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { vehicles } from "./vehicles";
+import { useCallback, useState } from "react";
+
+// const parkLayer: CircleLayer = {
+//   id: "landuse_park",
+//   type: "circle",
+//   paint: {
+//     "circle-radius": 100,
+//     "circle-color": "#00ffff",
+//     "circle-opacity": 0.4,
+//   },
+// };
+
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiaGVucmlrLWJyYXRoZW4iLCJhIjoiY2xsbm5wcnQxMDI1bDNkbzQxaTFnNDA2OSJ9.IjsrKGbU65mmJI-Ba-Ztug";
+const OSLO_BOUNDS = { longitude: 10.747263, latitude: 59.926678 };
+
+export default function MapContainer() {
+  const [viewState, setViewState] = useState({
+    ...OSLO_BOUNDS,
+    zoom: 13,
+  });
+  const [marker, setMarker] = useState({
+    ...OSLO_BOUNDS,
+  });
+  const [events, logEvents] = useState<Record<string, LngLat>>({});
+
+  const onMarkerDragStart = useCallback((event: MarkerDragEvent) => {
+    logEvents((_events) => ({ ..._events, onDragStart: event.lngLat }));
+  }, []);
+
+  const onMarkerDrag = useCallback((event: MarkerDragEvent) => {
+    logEvents((_events) => ({ ..._events, onDrag: event.lngLat }));
+
+    setMarker({
+      longitude: event.lngLat.lng,
+      latitude: event.lngLat.lat,
+    });
+  }, []);
+
+  const onMarkerDragEnd = useCallback((event: MarkerDragEvent) => {
+    logEvents((_events) => ({ ..._events, onDragEnd: event.lngLat }));
+  }, []);
+
+  // console.info(events);
+
+  return (
+    <div className="h-screen">
+      <Map
+        {...viewState}
+        mapboxAccessToken={MAPBOX_TOKEN}
+        onMove={(evt) => setViewState(evt.viewState)}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        minZoom={11}
+        attributionControl={false}
+      >
+        {vehicles.map((vehicle, key) => {
+          return (
+            <Marker
+              key={key}
+              longitude={vehicle.location.longitude}
+              latitude={vehicle.location.latitude}
+              color="red"
+            />
+          );
+        })}
+        <Marker
+          longitude={marker.longitude}
+          latitude={marker.latitude}
+          draggable
+          onDragStart={onMarkerDragStart}
+          onDrag={onMarkerDrag}
+          onDragEnd={onMarkerDragEnd}
+        />
+        {/* <Source
+            type="geojson"
+            data={{
+              type: "FeatureCollection",
+              features: [
+                {
+                  type: "Feature",
+                  properties: {},
+                  geometry: {
+                    coordinates: [marker.longitude, marker.latitude],
+                    type: "Point",
+                  },
+                },
+              ],
+            }}
+          >
+            <Layer {...parkLayer} />
+          </Source> */}
+      </Map>
+    </div>
+  );
+}
